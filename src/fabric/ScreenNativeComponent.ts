53428592d38548ba1c106907c91edc33efa0e486
Kacper Kafara <kacperkafara@gmail.com>
2022-09-27 11:20:26 +0200

    style: apply linter suggestion

--- src/fabric/ScreenNativeComponent.ts
+++ src/fabric/ScreenNativeComponent.ts
@@ -11,20 +11,20 @@ import type {
 type ScreenEvent = Readonly<{}>;
 
 type ScreenDismissedEvent = Readonly<{
-  dismissCount: Int32,
+  dismissCount: Int32;
 }>;
 
 type TransitionProgressEvent = Readonly<{
-  progress: Double,
-  closing: Int32,
-  goingForward: Int32,
+  progress: Double;
+  closing: Int32;
+  goingForward: Int32;
 }>;
 
 type GestureResponseDistanceType = Readonly<{
-  start: Float,
-  end: Float,
-  top: Float,
-  bottom: Float,
+  start: Float;
+  end: Float;
+  top: Float;
+  bottom: Float;
 }>;
 
 type StackPresentation =
@@ -52,36 +52,36 @@ type SwipeDirection = 'vertical' | 'horizontal';
 type ReplaceAnimation = 'pop' | 'push';
 
 export interface NativeProps extends ViewProps {
-  onAppear?: BubblingEventHandler<ScreenEvent>,
-  onDisappear?: BubblingEventHandler<ScreenEvent>,
-  onDismissed?: BubblingEventHandler<ScreenDismissedEvent>,
-  onNativeDismissCancelled?: BubblingEventHandler<ScreenDismissedEvent>,
-  onWillAppear?: BubblingEventHandler<ScreenEvent>,
-  onWillDisappear?: BubblingEventHandler<ScreenEvent>,
-  onTransitionProgress?: BubblingEventHandler<TransitionProgressEvent>,
-  customAnimationOnSwipe?: boolean,
-  fullScreenSwipeEnabled?: boolean,
-  homeIndicatorHidden?: boolean,
-  preventNativeDismiss?: boolean,
-  gestureEnabled?: WithDefault<boolean, true>,
-  statusBarColor?: ColorValue,
-  statusBarHidden?: boolean,
-  screenOrientation?: string,
-  statusBarAnimation?: string,
-  statusBarStyle?: string,
-  statusBarTranslucent?: boolean,
-  gestureResponseDistance?: GestureResponseDistanceType,
-  stackPresentation?: WithDefault<StackPresentation, 'push'>,
-  stackAnimation?: WithDefault<StackAnimation, 'default'>,
-  transitionDuration?: WithDefault<Int32, 350>,
-  replaceAnimation?: WithDefault<ReplaceAnimation, 'pop'>,
-  swipeDirection?: WithDefault<SwipeDirection, 'horizontal'>,
-  hideKeyboardOnSwipe?: boolean,
-  activityState?: WithDefault<Float, -1.0>,
-  navigationBarColor?: ColorValue,
-  navigationBarHidden?: boolean,
-  nativeBackButtonDismissalEnabled?: boolean,
-  onHeaderBackButtonClicked?: BubblingEventHandler<ScreenEvent>,
+  onAppear?: BubblingEventHandler<ScreenEvent>;
+  onDisappear?: BubblingEventHandler<ScreenEvent>;
+  onDismissed?: BubblingEventHandler<ScreenDismissedEvent>;
+  onNativeDismissCancelled?: BubblingEventHandler<ScreenDismissedEvent>;
+  onWillAppear?: BubblingEventHandler<ScreenEvent>;
+  onWillDisappear?: BubblingEventHandler<ScreenEvent>;
+  onTransitionProgress?: BubblingEventHandler<TransitionProgressEvent>;
+  customAnimationOnSwipe?: boolean;
+  fullScreenSwipeEnabled?: boolean;
+  homeIndicatorHidden?: boolean;
+  preventNativeDismiss?: boolean;
+  gestureEnabled?: WithDefault<boolean, true>;
+  statusBarColor?: ColorValue;
+  statusBarHidden?: boolean;
+  screenOrientation?: string;
+  statusBarAnimation?: string;
+  statusBarStyle?: string;
+  statusBarTranslucent?: boolean;
+  gestureResponseDistance?: GestureResponseDistanceType;
+  stackPresentation?: WithDefault<StackPresentation, 'push'>;
+  stackAnimation?: WithDefault<StackAnimation, 'default'>;
+  transitionDuration?: WithDefault<Int32, 350>;
+  replaceAnimation?: WithDefault<ReplaceAnimation, 'pop'>;
+  swipeDirection?: WithDefault<SwipeDirection, 'horizontal'>;
+  hideKeyboardOnSwipe?: boolean;
+  activityState?: WithDefault<Float, -1.0>;
+  navigationBarColor?: ColorValue;
+  navigationBarHidden?: boolean;
+  nativeBackButtonDismissalEnabled?: boolean;
+  onHeaderBackButtonClicked?: BubblingEventHandler<ScreenEvent>;
 };
 
 export default codegenNativeComponent<NativeProps>('RNSScreen', {
